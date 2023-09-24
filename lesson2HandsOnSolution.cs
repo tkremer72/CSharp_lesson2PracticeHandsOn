79b6ddc90705e40720b7f54608019d2dec2b6855
Thomas Kremer <tbone7243@gmail.com>
2023-09-24 16:32:08 -0400

    Solution commit.  Added the lesson 2 hands on solution to the project, should be the last commit and push.

---
+++ lesson2HandsOnSolution.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace lesson2HandsOn
+     {
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int age = 3;
+            bool isStudent = true;
+
+            if (age >= 65)
+            {
+                Console.WriteLine("Please pay $7");
+            }
+            else if (age < 0)
+            {
+                Console.WriteLine("Invalid age.");
+            }
+            else if (age <= 12 || isStudent)
+            {
+                Console.WriteLine("Please pay $8");
+            }
+            else
+            {
+                Console.WriteLine("Please pay $10");
+            }
+        }
+    }
+}
