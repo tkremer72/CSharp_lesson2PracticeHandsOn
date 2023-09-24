ec18c289f7f9982fda2a10e9d2db4cd1ff3d6405
Thomas Kremer <tbone7243@gmail.com>
2023-09-24 16:18:03 -0400

    Third commit.  Second part of the lesson 2 practice hands on complete.

--- Lesson2PracticeHandsOn.md
+++ Lesson2PracticeHandsOn.md
@@ -21,3 +21,29 @@
 
 # Tip!
      Print the ticket prices to the console.
+
+# Part 2
+#    Add the following to your program that prints the ticket prices for a user:
+
+     Create a new variable named isStudent that is of type boolean.
+     Set this to true to test your code.
+     Add another condition to the statement(s) checking for the age of a child. It should now be the following:
+     If the user is 12 years of age or younger OR a student, they receive a discounted price of $8 per ticket.
+     Age Range	Price
+     Student	$8
+     0-12	$8
+     13-64	$10
+     65 & Above	$7
+#     Starter Code:
+
+int age;
+
+// if senior citizen, pay $7
+
+// if student, pay $8
+
+// if child, pay $8
+
+// everyone else, pay $10
+
+
--- Program.cs
+++ Program.cs
@@ -6,16 +6,23 @@ namespace lesson2HandsOn
 {
     private static void Main(string[] args)
     {
-        int age = 25; //set the value of age to 25
-        double price = 0; //declare a double variable for price and initialize it to 0
+          //Part 1 add a value of your choice to the variable age
+        int age = 25;
+          //Part 2 Declare a boolean variable isStudent and give it a value
+          bool isStudent = true;
+          //Part 1 create a double variable price and initialize it to 0
+        double price = 0; 
 
-        if(age >= 65) {//if senior citizen, pay $7
+          //Part 1 if the age is greater than or equal to 65, pay $7
+        if(age >= 65) {
           price = 7;
         }
-        else if(age <= 12) { //if 12 or younger, pay $*
+        //Part 1 if the age is less than or equal to 12, pay $8
+        else if(isStudent || age <= 12) {  //Part 2 include isStudent in this check
           price = 8;
         }
-        else { //everyone else pay $10
+        //Part 1 Everyone else, pay $10
+        else { 
           price = 10;
         }
         Console.WriteLine("Your ticket price is $" + price);
