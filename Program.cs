429529ef09aedfe160817c3f0e521baf0f7fe033
Thomas Kremer <tbone7243@gmail.com>
2023-09-24 15:41:37 -0400

    Initial commit.  This is the first project commit.  Created the base program, staged and committed it.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson2HandsOn
+{
+     public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+}
